88b5687a57fc58a1ee5561a142f629125d08bec7
hend <enghend25jan@yahoo.com>
2018-07-25 22:54:52 +0300

    add project files

--- Sebha/Sebha/ViewController.swift
+++ Sebha/Sebha/ViewController.swift
@@ -10,59 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var currentCount = 0.0
+    var currentCount = 0
+    var currentDigitCount = 2
+        var currentIndexCount = 0.0
     {
         didSet{
-                self.arabicOutput.isHidden = currentCount == 0.0
+            self.arabicOutput.isHidden = currentCount == 0
              self.arabicOutput.text = String(Int(currentCount)).replacedEnglishDigitsWithArabic
         }
     }
-    let maxCount = 19.0
+    let maxIndexCount = 99.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 //        circularProgressView.angle = 0
-//        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector((ViewController.handleTap(gestureRecognizer:))))
-//        self.circularProgressView.addGestureRecognizer(gestureRecognizer)
+        
+     //  print("fsdfs \(arabicOutput.font.pointSize)")
+      //  self.arabicOutput.text = String(Int(currentCount)).replacedEnglishDigitsWithArabic
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector((ViewController.handleTap(gestureRecognizer:))))
+        self.circularProgressView.addGestureRecognizer(gestureRecognizer)
+        arabicOutput.adjustsFontSizeToFitWidth = true
+    //    print("\(currentCount.digitCount)")
+      
     }
 
     @IBOutlet weak var arabicOutput: UILabel!
     
+    @IBOutlet weak var circularProgressView: KDCircularProgress!
+    @objc func handleTap(gestureRecognizer: UIGestureRecognizer) {
+      //  print("click")
     
-    @IBAction func dsfasd(_ sender: Any) {
-        if currentCount != maxCount {
-            currentCount += 1
-            let newAngleValue = newAngle()
-            circularProgressView.animate(toAngle: newAngleValue, duration: 0.5, completion: nil)
+         print(" now : \(currentCount)")
+        if (currentIndexCount == maxIndexCount) {
+             currentCount += 1
+            
+            resizeCounterLabel()
+            circularProgressView.animate(fromAngle: circularProgressView.angle, toAngle: 0, duration: 0.5, completion: nil)
+            currentIndexCount = 0
+            print("sdfdsf \(arabicOutput.font.pointSize)")
         }
         else{
             currentCount += 1
-            //currentCount = 0
-            circularProgressView.animate(fromAngle: circularProgressView.angle, toAngle: 0, duration: 0.5, completion: nil)
+            currentIndexCount += 1
+            let newAngleValue = newAngle()
+            circularProgressView.animate(toAngle: newAngleValue, duration: 0.5, completion: nil)
         }
     }
     
-    @IBOutlet weak var circularProgressView: KDCircularProgress!
-    @objc func handleTap(gestureRecognizer: UIGestureRecognizer) {
-        print("inside circle")
-        if currentCount != maxCount {
-            currentCount += 1
-            let newAngleValue = newAngle()
-            circularProgressView.animate(toAngle: newAngleValue, duration: 0.5, completion: nil)
-        }
-        else{
-            currentCount = 0
-            circularProgressView.animate(fromAngle: circularProgressView.angle, toAngle: 0, duration: 0.5, completion: nil)
+    func resizeCounterLabel(){
+        if currentCount.digitCount > currentDigitCount{
+            currentDigitCount = currentCount.digitCount
+            if arabicOutput.font.pointSize != 35{
+                arabicOutput.font = UIFont.systemFont(ofSize: arabicOutput.font.pointSize - 5, weight:.regular)
+                arabicOutput.sizeToFit()
+            }
         }
     }
 
     func newAngle() -> Double {
-        return 360 * (currentCount / maxCount)
+        return 360 * (currentIndexCount / maxIndexCount)
     }
     
     @IBAction func gfgf(_ sender: Any) {
         currentCount = 0
+        currentIndexCount = 0
+        currentDigitCount = 2
         circularProgressView.animate(fromAngle: circularProgressView.angle, toAngle: 0, duration: 0.5, completion: nil)
     }
     
